8158da4ba40906f7a5344e27e0d055d9af132b14
Pedro Henrique <phd19994@gmail.com>
2022-08-12 20:32:03 -0300

    all components prototype finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserCardComponent } from './user-card/user-card.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCardComponent,
+    UserListComponent,
+    NavigationMenuComponent
   ],
   imports: [
     BrowserModule
